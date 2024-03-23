7c0e4f2736e9c02482cb398a14031fa9fb2908e7
P-AlaKara <alakara11728@gmail.com>
2024-03-23 15:06:11 +0300

    query testing complete

--- connectivity/main.py
+++ connectivity/main.py
@@ -1,6 +1,14 @@
+from flask import Flask, request, jsonify, send_file
 import psycopg2
 from config import config
-def connect():
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+@app.route('/api/products', methods=['GET'])
+def get_products():
     connection = None
     try:
         params = config()
@@ -9,17 +17,24 @@ def connect():
 
         # create a cursor
         crsr = connection.cursor()
-        print('PostgreSQL database version: ')
-        crsr.execute('SELECT * FROM customers')
-        db_version = crsr.fetchone()
-        print(db_version)
-        crsr.close()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if connection is not None:
-            connection.close()
-        print('Database connection terminated.')
+        #get query parameters from the request
+        columns = request.args.get('columns')
+        criteria = request.args.get('criteria')
+        query = f"SELECT {columns} FROM inventory"
+        if criteria:
+            query += f" WHERE {criteria}"
 
+        # Execute the SQL query
+        crsr.execute(query)
+        # Fetch all rows
+        products = crsr.fetchall()
+        crsr.close()
+        connection.close()
+        #convert the fetched data to JSON format and return as response
+        return jsonify(products)
+    except psycopg2.Error as e:
+        return jsonify({'error': str(e)}),500
+    except Exception as e:
+        return jsonify({'error': str(e)}),500
 if __name__ == "__main__":
-    connect()
+    app.run(debug=True)
