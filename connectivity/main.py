c980497394fcc8f78850676b579ff516b32d197e
P-AlaKara <alakara11728@gmail.com>
2024-03-26 23:18:24 +0300

    insert and delete functions drafted

--- connectivity/main.py
+++ connectivity/main.py
@@ -31,7 +31,7 @@ def get_data():
         query = f"SELECT {columns} FROM {table}" + criteria_query
 
         # Use a prepared statement with placeholder for criteria_constraint
-        print(query)
+        logging.debug(query)
         crsr.execute(query, (criteria_constraint,))
 
         # Fetch all rows
@@ -44,5 +44,55 @@ def get_data():
         return jsonify({'error': str(e)}),500
     except Exception as e:
         return jsonify({'error': str(e)}),500
+
+@app.route('/api/insert/products', methods=['POST'])
+def insert_product():
+    try:
+        params = config()
+        connection = psycopg2.connect(**params)
+        crsr = connection.cursor()
+
+        # Extract data from request body
+        data = request.json  
+        # Construct SQL INSERT query
+        query = "INSERT INTO inventory (productID, category, sub_category, stock) VALUES (%s, %s, %s, %d)"
+        values = (data['productID'], data['category'], data['subCategory'], int(data['stock']))
+        logging.debug(values)
+        # Execute SQL query
+        crsr.execute(query, (values))
+        connection.commit()
+
+        crsr.close()
+        connection.close()
+        # Return success message
+        return jsonify({'message': 'Product inserted successfully'}), 200
+    except psycopg2.Error as e:
+        return jsonify({'error': str(e)}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}),500
+
+@app.route('/api/delete/product/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    try:
+        # Establish a connection to the database
+        params = config()
+        connection = psycopg2.connect(**params)
+        cursor = connection.cursor()
+
+        # Construct the SQL query to delete the product
+        query = "DELETE FROM inventory WHERE productID = %s"
+        
+        cursor.execute(query, (product_id,))
+        connection.commit()
+
+        # Close cursor and connection
+        cursor.close()
+        connection.close()
+
+        # Return a success message
+        return jsonify({'message': f'Product with ID {product_id} deleted successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}),500
+                        
 if __name__ == "__main__":
     app.run(debug=True)
